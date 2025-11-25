446788e926493daa4b8f0883520ed55fda7298c0
Htan <oxygiang123@gmail.com>
2025-11-26 03:44:15 +0700

    init app, create dang_nhap page

---
+++ android/app/src/main/kotlin/com/example/smartchef/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.smartchef
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'page/dang_nhap.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Smart Chef',
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: const LoginPage(),
+    );
+  }
+}
